0a75a2f4af26fa104cd5d58ffa96e088ffaab470
Turaliev-KI17 <74761470+Turaliev-KI17@users.noreply.github.com>
2022-06-06 02:52:22 +0700

    Add files via upload

---
+++ WeaponType/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace WeaponType
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+           
+
+            
+        }
+        private void OpenImg(object Sender, RoutedEventArgs E)
+        {
+            var dialog = new OpenFileDialog
+            {
+                Title = "Выбор изображения",
+                CheckFileExists = true
+
+            };
+
+            if (dialog.ShowDialog(this) != true) return;
+            var fileName = dialog.FileName;
+            ImageView.Source= new BitmapImage(new Uri(fileName, UriKind.RelativeOrAbsolute));
+            // Create single instance of sample data from first line of dataset for model input
+            WeaponTypeClassificator.ModelInput sampleData = new WeaponTypeClassificator.ModelInput()
+            {
+                ImageSource = fileName,
+            };
+
+            // Make a single prediction on the sample data and print results
+            var predictionResult = WeaponTypeClassificator.Predict(sampleData);
+            InitializeComponent();
+            Result.Text = $"{predictionResult.Prediction} - {predictionResult.Score.Max():p0}";
+
+        }
+    }
+}
